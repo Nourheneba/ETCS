e5cc2122a18bfc3f7be311440370182e1c400127
Nourheneba <nourhenebenachoura@gmail.com>
2019-07-10 10:41:23 +0200

    Update CellGeneration.py

--- CellGeneration.py
+++ CellGeneration.py
@@ -37,7 +37,7 @@ distance_y = Ymax - Ymin
 
 # fixe step
 stpx = 0.07
-stpy = 0.85
+stpy = 0.55
 
 # nbr Cells
 nbCellX = int(math.ceil(distance_x / stpx))
@@ -53,6 +53,7 @@ w, h = nbCellX, nbCellY
 matrix = [[0 for y in range(h)] for x in range(w)]
 i = 0
 
+#********************************Découpage en cellules*************************
 for p in dataset:
     calcX = p[0]
     valeurXFromRef = calcX - Xmin
@@ -67,20 +68,15 @@ for p in dataset:
     y = divmod(valeurYFromRef, stpy)
     celluleY = int(y[0])
     
-#    print("celluleX",celluleX)
-#    print("celluleY",celluleY)
-
-    
     #print("cellule (X,Y;Z)  (", celluleX ,",", celluleY ,";", calcZ,")\n")
     celluleCourante = matrix[celluleX][celluleY]
  
     if (celluleCourante == 0):
         # if cell is empty
         cellule = [None] * 6
-        # liste des points
-        
+        # liste des points      
         listePoints = []
-        # listePoints.append(p)
+        
         listePoints.append((i, calcX, calcY, calcZ))
         cellule[0] = listePoints
         # Zmin
@@ -98,13 +94,11 @@ for p in dataset:
         matrix[celluleX][celluleY] = cellule
 
     else:
-        # Sinon on compare le Z courant a celui de la cellule
-        # Si le Z courant est superieur au Z de la cellule
-        # On met le point courant dans la cellule
+
         listePoints = celluleCourante[0]
         zMinCellule = celluleCourante[1]
         zMaxCellule = celluleCourante[2]
-        # listePoints.append(p)
+
         listePoints.append((i, calcX, calcY,calcZ))
         celluleCourante[0] = listePoints
         if (calcZ > zMaxCellule):
@@ -115,67 +109,134 @@ for p in dataset:
         celluleCourante[3] = celluleCourante[3] + 1
         # Somme des Z
         celluleCourante[4] = celluleCourante[4] + calcZ
-        
         # Moyenne Z
         celluleCourante[5] = celluleCourante[4]/celluleCourante[3]
         matrix[celluleX][celluleY] = celluleCourante      
         
     i = i + 1
     
-    
+#******************************************************************************    
 
-pointsIndex = []
-resIndex = []
-#
-#
-##for indexCelluleX in range(nbCellX - 1):
-indexCelluleY = 13
-#   
+
+
+#****************Calcule de la Zmoyenne Max et Min de la dalle*****************
+
+sumGeneralMax = 0
+sumGeneralMin = 0
+k = 0
+
+for indexCelluleX in range(nbCellX):
+    for indexCelluleY in range(nbCellY):
+        cell = matrix[indexCelluleX][indexCelluleY]
+        if cell != 0 :
+            k = k + 1
+            sumGeneralMax = sumGeneralMax + cell[2]
+            sumGeneralMin = sumGeneralMin + cell[1]
+moyGeneralMax =  sumGeneralMax/k     
+moyGeneralMin =  sumGeneralMin/k   
+
+print("moyGeneralMax",moyGeneralMax)       
+print("moyGeneralMin",moyGeneralMin)           
+        
+#******************************************************************************
+
+#***********Calcule de la Difference entre Z cellueSuiv et Z celluleCour*******
+indexCelluleY = 31
+
 d = np.array([0])
 y = np.array([0])
 yRes = np.array([0])
-#
+pointsIndex = []
+resIndex = []
+
 for indexCelluleX in range(nbCellX-1):
    
     celluleCourante = matrix[indexCelluleX][indexCelluleY]
     celluleSuiv = matrix[indexCelluleX+1][indexCelluleY]
     
     dtype = [('index', int), ('X', float), ('Y', float), ('Z', float)]
-    
-    
-    if celluleSuiv == 0 or celluleCourante == 0:
+
+    if celluleCourante == 0  :
+        
         diff = 0
         d = np.append(d, diff)
         y = np.append(y, indexCelluleX)
 
-    if celluleSuiv != 0 and celluleCourante != 0:
+    if celluleCourante != 0 and celluleSuiv != 0 :
         
         diff = celluleSuiv[5] - celluleCourante[5]
-#        print("Cellule (",indexCelluleX ,";",indexCelluleY,") : ",celluleCourante[5] , " Diff = " , diff,"\n" )
+#       print("Cellule (",indexCelluleX ,";",indexCelluleY,") : ",celluleCourante[5] , " Diff = " , diff,"\n" )
         pts = np.array(celluleCourante[0], dtype=dtype)
- 
+        
         d = np.append(d, diff)
         y = np.append(y, indexCelluleX)
-#        
+        
         for index in range(pts.size):
             c = pts[index]
-            print("Point",c)
-            pointsIndex.append(c[0])
-#        
-#        #print("diff",diff)
-#        
-#        if(diff >= 0.06 and diff <= 0.1 ):
+            if( c[3] <= moyGeneralMax ):
+                pointsIndex.append(c[0])  
+    
+            
+#        if(diff >= 0.01 and diff <= 0.14 ):
 #            dtype = [('index', int), ('X', float), ('Y', float), ('Z', float)]
 #            resPts = np.array(celluleCourante[0], dtype=dtype)
-#            resPts = np.array(celluleSuiv[0], dtype=dtype)
+#
 #            print("Res",resPts)
-##        
+#        
 #            for idx in range(resPts.size):
 #                p = resPts[idx]
 #                #print("Point",c)
 #                resIndex.append(p[0])
 #                
 #
+#******************************************************************************
+        
+#****************Comparer Zmoy courante a Zmoy des 5 cellules G/D *************
+indexCelluleY = 31
+for indexCelluleX in range(nbCellX-5):
+   
+    celluleCourante = matrix[indexCelluleX][indexCelluleY]
+    if(celluleCourante!=0):
+#        print("Cellule (",indexCelluleX ,";",indexCelluleY,") : ",celluleCourante[5] ,"\n" )
+    
+        sumLcell=0
+        sumRcell=0
+        moyRcell=[0]
+        moyLcell=[0]
+        #left side
+        for i in range(indexCelluleX-5,indexCelluleX) :
+#            print("i = ",i) 
+            if matrix[i][indexCelluleY] == 0:
+                continue
+            if matrix[i][indexCelluleY] != 0 :
+               cl = matrix[i][indexCelluleY]
+               sumLcell = (sumLcell + abs(cl[5]))
+               moyLcell = divmod(sumLcell, 5)
+#               print("moyLcell = ",moyLcell) 
+        #right side
+        for j in range(indexCelluleX+5,indexCelluleX, -1) :
+#            print("j = ",j)              
+            if matrix[j][indexCelluleY] == 0:
+                continue
+            if matrix[j][indexCelluleY] != 0 :
+                cr = matrix[j][indexCelluleY]
+                sumRcell = (sumRcell + abs(cr[5]))
+                moyRcell = divmod(sumRcell, 5)
+#                print("moyRcell = ",moyRcell)          
+
+        if(moyLcell[0] < 165 and moyRcell[0] < 165):
+            dtype = [('index', int), ('X', float), ('Y', float), ('Z', float)]
+            resPts = np.array(celluleCourante[0], dtype=dtype)
+
+#            print("Res",resPts)
+        
+            for idx in range(resPts.size):
+                p = resPts[idx]
+                #print("Point",c)
+                resIndex.append(p[0])
+
+#********************Enregistrer 2 fichiers Coupe et resultat******************     
+        
 _, ext = os.path.splitext(fileName)
 
 new_file = str(_+"_Coupe"+ext)
@@ -183,15 +244,13 @@ outFile2 = File.File(new_file, mode = "w", header = inFile.header)
 outFile2.points = point_records[pointsIndex]
 outFile2.close()
 
+res_file = str(_+"_Res31"+ext)
+outFile3 = File.File(res_file, mode = "w", header = inFile.header)
+outFile3.points = point_records[resIndex]
+outFile3.close()
+##
 
-#res_file = str(_+"_Res"+ext)
-#outFile3 = File.File(res_file, mode = "w", header = inFile.header)
-#outFile3.points = point_records[resIndex]
-#outFile3.close()
-#
-#
-#
-# Drow graphe
+#****************************** Dessiner graphe *******************************
         
 app = tk.Tk()
 app.wm_title("Graphe Matplotlib")   
@@ -210,21 +269,7 @@ canvas = graph.get_tk_widget()
 canvas.grid(row=1, column=1)
 
 app.mainloop()
-
-
-#plt.show()   
             
-            #print("Cellule (",indexCelluleX ,";",indexCelluleY,") : ",celluleCourante[5] ,"\n" )
-#            if (celluleC > 8 and celluleC < 16 ) :
-#                
-#                dtype = [('index', int), ('X', float), ('Y', float), ('Z', float)]
-#                pts = np.array(celluleCourante[0], dtype=dtype)
-#                
-#                for index in range(pts.size):
-#                    c = pts[index]
-#                    listeIndexs.append(c[0])
-#                
-#               
 
             
 
